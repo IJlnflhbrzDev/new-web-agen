70b1afd0c726521a203ed348099189bad9683d60
unknown <ijlnflhbrzdev@gmail.com>
2021-10-26 21:08:28 +0700

    inprogress cart ui

--- script.js
+++ script.js
@@ -65,3 +65,95 @@ function ChngeWidthProduk() {
           elemet.classList.toggle("c-col-3");
      });
 }
+
+
+// NUMBER FORMAT START
+const numberFormat = (value) => {
+     if (value.toString()[0] == "-") {
+          var negative = "-";
+     } else {
+          negative = "";
+     }
+     var raw = value.toString().replace(/(?!\.)\D/g, "").split(".");
+     var whole = raw[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+     var decimal = false;
+     if (raw.length > 1) {
+          decimal = raw[1];
+     }
+     if (decimal !== false && (decimal !== "0" || decimal !== "00")) {
+          return negative + whole + "." + decimal;
+     } else {
+          return negative + whole;
+     }
+}
+// NUMBER FORMAT START
+
+
+
+// SECTION DOM PRICE & STOK START
+
+let price = parseInt(document.getElementById("inputHargaHidden").value);
+let availableStok = parseInt(document.getElementById("inputStokHidden").value);
+
+const calculatePrice = (action) => {
+     let tagStok = document.getElementById("valueStok");
+     let tagPrice = document.getElementById("valueTotalPrice");
+     let nowStok = parseInt(tagStok.innerText);
+
+     if (action == "minus") {
+          nowStok--;
+
+          if (nowStok < 1) {
+               alert("Stok tidak boleh dibawah satu!");
+               tagStok.innerText = 1;
+               nowStok = 1;
+          } else {
+               tagStok.innerText = nowStok;
+          }
+     } else {
+          nowStok++;
+
+          if (nowStok > availableStok) {
+               alert("Stok melebihi jumlah yang tersedia");
+               nowStok = availableStok;
+          } else {
+               tagStok.innerText = nowStok;
+          }
+     }
+
+     let totalPrice = price * nowStok;
+     // console.log(price);
+     tagPrice.innerText = `Rp. ${numberFormat(totalPrice)}`;
+}
+
+document.getElementById("btnPlusStok").addEventListener("click", function () {
+     calculatePrice("plus")
+});
+document.getElementById("btnMinusStok").addEventListener("click", function () {
+     calculatePrice("minus")
+});
+
+// SECTION DOM PRICE & STOK END
+
+
+
+
+
+// EVENT CLICK CHANGE SRC IMAGE LARGE START
+const container_produk_image = document.querySelector(".produk-image");
+const imgLarge = document.querySelector(".thumbnail");
+
+container_produk_image.addEventListener("click", function (e) {
+     // membuat kondisi ketika event click container img ini meng klik sebuah salah satu gambar small
+     if (e.target.className == "thumb-small") {
+          imgLarge.src = e.target.src;
+          imgLarge.classList.add("fade");
+
+          setTimeout(() => {
+               imgLarge.classList.remove("fade")
+          }, 500);
+     }
+
+});
+// EVENT CLICK CHANGE SRC IMAGE LARGE END
+
